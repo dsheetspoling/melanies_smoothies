a41cece24f8b8ffc95d1c50dadb16081ca90dc66
dsheetspoling <dsheetspoling@gmail.com>
2025-06-19 11:22:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -35,10 +36,11 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     st.write(ingredients_list)
     st.text(ingredients_list)
-
     ingredients_string = ''
 ## added space below here
     for fruit_chosen in ingredients_list:
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         ingredients_string += fruit_chosen + ' '
 
     st.write(ingredients_string)
@@ -57,7 +59,4 @@ if ingredients_list:
         
      st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
