e7e0201dcd7abfbaf4e734702ebcbeed805c1caa
dsheetspoling <dsheetspoling@gmail.com>
2025-06-19 11:43:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,14 @@ st.write("The name on your smoothie will be:", name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
+
+
+
 name_on_order_list = [name_on_order]
 
 ingredients_list = st.multiselect(
